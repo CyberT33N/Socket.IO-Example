87426910f5af922f19f49401407924fef9be22cb
ᑕƳᗷᗴᖇ丅33ᑎ™  ㋛⎠ <17134633+CyberT33N@users.noreply.github.com>
2020-11-18 00:07:17 +0100

    Delete controller-endpoints.js

--- controller/controller-endpoints.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-const endpoints = require('../services/endpoints');
-
-const controller = {
-  getUserDetails: async (req, res)=>{ return await endpoints.getUserDetails(req, res); },
-  getRoomDetails: async (req, res)=>{ return await endpoints.getRoomDetails(req, res); }
-};
-
-module.exports = controller;
