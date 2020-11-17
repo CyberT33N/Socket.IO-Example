bad98a4e08ac759e159093ea73d1378f52aff558
ᑕƳᗷᗴᖇ丅33ᑎ™  ㋛⎠ <17134633+CyberT33N@users.noreply.github.com>
2020-11-18 00:07:10 +0100

    Delete controller-bot.js

--- controller/controller-bot.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-const bot = require('../services/bot');
-
-const controller = {
-  startBROWSER: async ()=>{ return await bot.startBROWSER(); },
-  openLink: async (page, link)=>{ return await bot.openLink(page, link); }
-};
-
-module.exports = controller;
