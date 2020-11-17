f953b44251a23360a475b0e28d0108f008377f66
ᑕƳᗷᗴᖇ丅33ᑎ™  ㋛⎠ <17134633+CyberT33N@users.noreply.github.com>
2020-11-18 00:07:29 +0100

    Delete controller.js

--- controller/controller.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-const socketio = require('../services/socketio');
-
-const controller = {
-  rootConnect: async (http)=>{ return await socketio.rootConnect(http); }
-};
-
-module.exports = controller;
