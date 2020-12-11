62f67b3bd90000262f154f392e8292277d3bd2ef
CyberT33N <resets.17134633+CyberT33N@users.noreply.github.com>
2020-12-11 01:02:48 +0100

    Linter service exposefunctions.mjs

--- controller/utils/exposefunctions.mjs
+++ controller/utils/exposefunctions.mjs
@@ -2,20 +2,34 @@ import * as expose from '../../services/utils/exposefunctions.mjs';
 
 export default {
 
-  config: async page=>{ return await new expose.GetData().config(page); },
+  config: async page=>{return await new expose.GetData().config(page);},
 
   // ---- exposeFunctionsWeb() ----
-  checkURLParameter: async pptr=>{ return await new expose.CheckDOM().urlParameter(pptr); },
-  checkPartnerMessage: async pptr=>{ return await new expose.CheckDOM().partnerMessage(pptr); },
+  checkURLParameter: async pptr=>{
+    return await new expose.CheckDOM().urlParameter(pptr);
+  },
+  checkPartnerMessage: async pptr=>{
+    return await new expose.CheckDOM().partnerMessage(pptr);
+  },
 
   // ---- exposeFunctionsReq() ----
-  details: async page=>{ return await new expose.GetData().details(page); },
+  details: async page=>{
+    return await new expose.GetData().details(page);
+  },
 
   // ---- exposeFunctionsSocket() ----
-  listenerChatMessage: async (pptr, devIO)=>{ return await new expose.Listener(pptr, devIO).chatMessage(); },
-  listenerRoomConnect: async (pptr, devIO)=>{ return await new expose.Listener(pptr, devIO).roomConnect(); },
+  listenerChatMessage: async (pptr, devIO)=>{
+    return await new expose.Listener(pptr, devIO).chatMessage();
+  },
+  listenerRoomConnect: async (pptr, devIO)=>{
+    return await new expose.Listener(pptr, devIO).roomConnect();
+  },
 
-  checkTimeCSS: async (pptr, socket, socketPartner)=>{ return await new expose.Listener(pptr).checkTimeCSS(socket, socketPartner); },
-  incomeMsg: async (pptr, devIO)=>{ return await new expose.Listener(pptr, devIO).incomeMsg(); }
+  checkTimeCSS: async (pptr, socket, socketPartner)=>{
+    return await new expose.Listener(pptr).checkTimeCSS(socket, socketPartner);
+  },
+  incomeMsg: async (pptr, devIO)=>{
+    return await new expose.Listener(pptr, devIO).incomeMsg();
+  },
 
-}
+};
--- services/utils/exposefunctions.mjs
+++ services/utils/exposefunctions.mjs
@@ -1,179 +1,161 @@
-/*################ Controller ################*/
+/* ################ Controller ################ */
 import controller from '../../controller/socketio.mjs';
 import controllerBot from '../../controller/bot.mjs';
 import controllerLib from '../../controller/lib.mjs';
 import controllerMongoDB from '../../controller/mongodb.mjs';
 
-/*################ Logs ################*/
+/* ################ Logs ################ */
 import log from 'fancy-log';
-import chalkAnimation from 'chalk-animation';
-import gradient from 'gradient-string';
-import chalk from 'chalk';
 
 
-
-
-export class GetData{
-
-  async details(page){ log('class GetData - details()');
-    await page.exposeFunction('details', async()=>{ log('--- EXPOSE - details ----');
+export class GetData {
+  async details(page) {
+    await page.exposeFunction('details', async ()=>{
       const config = controllerLib.getConfig();
+      const room = config.test.room;
+      const token = config.test.user[0].token;
       return {
-        testRoomDetails: await controllerMongoDB.getRoomDetails(config.test.room),
-        testUserDetails: await controllerMongoDB.getUserDetails(config.test.user[0].token)
+        testRoomDetails: await controllerMongoDB.getRoomDetails(room),
+        testUserDetails: await controllerMongoDB.getUserDetails(token),
       };
     }); // await pptr.page.exposeFunction('details', async()=>{
   }; // async details(page){
 
 
-  async config(page){ log('class GetData - config()');
-    await page.exposeFunction('config', ()=>{ log('--- EXPOSE - config() ----');
-        return controllerLib.getConfig();
-    }); // await pptr.page.exposeFunction('config', ()=>{
-  }; // async config(page){
-
+  async config(page) {
+    await page.exposeFunction('config', ()=>{
+      return controllerLib.getConfig();
+    }); // await page.exposeFunction('config', ()=>{
+  }; // async config(page) {
 }; // export class getData(){
 
 
-
-
-
-
-
-
-
-
-
-
-export class CheckDOM{
-
-  constructor(){
+export class CheckDOM {
+  constructor() {
     const config = controllerLib.getConfig();
     this.hostFull = config.test.hostFull;
     this.linkPartner = config.test.linkPartner;
   }; // constructor(){
 
-  async urlParameter(pptr){ log('checkURLParameter()');
-    await pptr.page.exposeFunction('checkURLParameter', async script =>{ log('--- EXPOSE - checkURLParameter - script: ' + script + '---');
-
-      const newTab = await controllerBot.openLinkNewTab(pptr.client, this.hostFull + '/?usertoken=');
+  async urlParameter(pptr) {
+    await pptr.page.exposeFunction('checkURLParameter', async script=>{
+      const newTab = await controllerBot.openLinkNewTab(
+          pptr.client,
+          this.hostFull + '/?usertoken=',
+      );
 
       // execute script inside of DOM by creating new function and run it
       return await newTab.evaluate(async script=>{
-        var f = new Function('return ' + script)();
+        const f = new Function('return ' + script)();
         return f();
-      }, script); // const d = await newTab.evaluate(async script=>{
+      }, script); // await newTab.evaluate(async script=>{
+    }); // await pptr.page.exposeFunction('checkURLParameter', async script=>{
+  }; // async urlParameter(pptr) {
 
-    }); // await pptr.page.exposeFunction('checkURLParameter', ()=>{
-  }; // async checkURLParameter(pptr){
 
-
-  async partnerMessage(pptr){ log('checkPartnerMessage()');
-    await pptr.page.exposeFunction('checkPartnerMessage', async link =>{ log('--- EXPOSE - checkPartnerMessage ----');
-
-      // do not delete timeout - We will wait here until animation is ready
-      const newTab = await controllerBot.openLinkNewTab(pptr.client, this.linkPartner, 2000);
+  async partnerMessage(pptr) {
+    await pptr.page.exposeFunction('checkPartnerMessage', async link=>{
+      const newTab = await controllerBot.openLinkNewTab(
+          pptr.client,
+          this.linkPartner,
+          2000,
+      );
 
       // check if message was recieved at partner
       return await newTab.evaluate(msg=>{
         const lastElement = document.querySelector('.chat div:last-child');
-        if(lastElement.textContent == msg &&
-        lastElement.getAttribute('class') == 'bubble you' ) return true;
+        if (
+          lastElement.textContent == msg &&
+          lastElement.getAttribute('class') == 'bubble you'
+        ) return true;
       }, 'sample_message123');
-
-    }); // await pptr.page.exposeFunction('checkPartnerMessage', async link =>{
-  }; // async checkPartnerMessage(pptr){
-
+    }); // await newTab.evaluate(msg=>{
+  }; // async partnerMessage(pptr) {
 }; // class CheckDOM extends CheckDOMLib{
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-class ListenerEvents{
-
-
-  async chatMessage(){ log('listenerChatMessage()');
-    await this.pptr.page.exposeFunction('listenerChatMessage', async ()=>{ log('EXPOSE - listenerChatMessage()');
-
+class ListenerEvents {
+  async chatMessage() {
+    await this.pptr.page.exposeFunction('listenerChatMessage', async ()=>{
       const newTab = await controllerBot.newTab(this.pptr.client);
-      const [msg] = await Promise.all([
-        this.createSocketListener('chat message', await this.createDevSocket(newTab, this.devIO)),
-        await controllerBot.typeText(newTab, 'textarea', 'sample_message123', 10),
-        await controllerBot.click('.write-link.send', newTab, 1000)
-      ]); return msg;
-
-    }); // await this.pptr.page.exposeFunction('listenerChatMessage', async ()=>{
+      const [msg] = await Promise.all(
+          [
+            this.createSocketListener(
+                'chat message',
+                await this.createDevSocket(newTab, this.devIO),
+            ),
+            await controllerBot.typeText(
+                newTab,
+                'textarea',
+                'sample_message123',
+                10,
+            ),
+            await controllerBot.click('.write-link.send', newTab, 1000),
+          ],
+      ); return msg;
+    }); // this.pptr.page.exposeFunction('listenerChatMessage', async ()=>{
   }; // async listenerChatMessage(){
 
 
-  async roomConnect(){ log('listenerRoomConnect()');
-    await this.pptr.page.exposeFunction('listenerRoomConnect', async ()=>{ log('EXPOSE - listenerRoomConnect()');
-
+  async roomConnect() {
+    await this.pptr.page.exposeFunction('listenerRoomConnect', async ()=>{
       const newTab = await controllerBot.newTab(this.pptr.client);
-      const [roomID] = await Promise.all([
-        this.createSocketListener('room connect', await this.createDevSocket(newTab, this.devIO)),
-        controllerBot.click('.person', newTab, 1000)
-      ]); return roomID;
+      const [roomID] = await Promise.all(
+          [
+            this.createSocketListener(
+                'room connect',
+                await this.createDevSocket(newTab, this.devIO),
+            ),
+            controllerBot.click('.person', newTab, 1000),
+          ],
+      ); return roomID;
+    }); // this.pptr.page.exposeFunction('listenerRoomConnect', async ()=>{
+  }; // async roomConnect() {
 
-    }); // await pptr.page.exposeFunction('listenerRoomConnect', link=>{
-  }; // async listenerRoomConnect(){
 
-
-  async checkTimeCSS(socket, socketPartner){ log('checkTimeCSS()');
-    await this.pptr.page.exposeFunction('checkTimeCSS', async ()=>{ log('EXPOSE - checkTimeCSS');
-
+  async checkTimeCSS(socket, socketPartner) {
+    await this.pptr.page.exposeFunction('checkTimeCSS', async ()=>{
       const msg = {
-        msg: "sample message22..",
+        msg: 'sample message22..',
         room: this.room,
         usertoken: this.partnerToken,
-        date: '12/34/5678, 12:34 pm'
+        date: '12/34/5678, 12:34 pm',
       };
 
       const [d] = await Promise.all([
         this.createSocketListener('msg', socket),
-        this.emitMsg(socketPartner, 'chat message', msg, 1000)
+        this.emitMsg(socketPartner, 'chat message', msg, 1000),
       ]); return d;
-
     }); // await this.pptr.page.exposeFunction('checkTimeCSS', async ()=>{
-  }; // async checkTimeCSS(socket, socketPartner){
+  }; // async checkTimeCSS(socket, socketPartner) {
 
 
-  async incomeMsg(){ log('incomeMsg()');
-    await this.pptr.page.exposeFunction('incomeMsg', async ()=>{ log('EXPOSE - incomeMsg');
-
+  async incomeMsg() {
+    await this.pptr.page.exposeFunction('incomeMsg', async ()=>{
       const newTab = await controllerBot.newTab(this.pptr.client);
 
       // emit sample message to trigger websocket
-      this.emitMsg(await this.createDevSocket(newTab, this.devIO), 'msg', 'new sample message');
+      this.emitMsg(
+          await this.createDevSocket(newTab, this.devIO),
+          'msg',
+          'new sample message',
+      );
 
       // check if message was recieved at partner
       return await newTab.evaluate(()=>{
         const lastElement = document.querySelector('.chat div:last-child');
-        if(lastElement.textContent == "new sample message" &&
-        lastElement.getAttribute('class') == 'bubble you' ) return true;
+        if (
+          lastElement.textContent == 'new sample message' &&
+          lastElement.getAttribute('class') == 'bubble you'
+        ) return true;
       }); // return await newTab.evaluate(()=>{
-
-    }); // await pptr.page.exposeFunction('incomeMsg', link=>{
+    }); // await this.pptr.page.exposeFunction('incomeMsg', async ()=>{
   }; // async incomeMsg(){
-
 }; // class ListenerEvents{
 
 
-
-
-export class Listener extends ListenerEvents{
-
-  constructor(pptr, devIO){ log('class Listener - constructor()');
+export class Listener extends ListenerEvents {
+  constructor(pptr, devIO) {
     super();
 
     this.pptr = pptr;
@@ -185,22 +167,26 @@ export class Listener extends ListenerEvents{
     this.partnerToken = config.test.user[1].token;
   }; // constructor(pptr, devIO){
 
-  async createDevSocket(page, devIO){ log('createDevSocket()');
+  async createDevSocket(page, devIO) {
     const [devSocket] = await Promise.all([
       controller.rootConnect(devIO),
-      controllerBot.openLink(page, this.linkClient)
+      controllerBot.openLink(page, this.linkClient),
     ]); return devSocket;
-  }; // async createDevSocket(){
+  }; // async createDevSocket(page, devIO) {
 
-  createSocketListener(name, socket){ return new Promise(resolve => { log(`createSocketListener() - Listener Name: ${name}`);
-    socket.on(name, data=>{ log(`createSocketListener() - socket.on() - data: ${JSON.stringify(data, null, 4)}`);
+  createSocketListener(name, socket) {return new Promise(resolve=>{
+    socket.on(name, data=>{
       resolve(data);
-     }); // socket.on(name, data=>{
-  })}; // createSocketListener(name, socket){
+    }); // socket.on(name, data=>{
+  });}; // createSocketListener(name, socket){
 
-  async emitMsg(socket, name, msg, delay){ log(`emitMsg() - Listener Name: ${name} - Message: ${JSON.stringify(msg, null, 4)} - Delay: ${delay}`);
-    if(delay) await new Promise(resolve => setTimeout(resolve, delay));
+  async emitMsg(socket, name, msg, delay) {
+    log(`
+      emitMsg() - Listener Name: ${name} - Delay: ${delay}
+      Message: ${JSON.stringify(msg, null, 4)}
+    `);
+
+    if (delay) await new Promise(resolve=>setTimeout(resolve, delay));
     socket.emit(name, msg);
-  }; // emitMsg(socket){
-
+  }; // async emitMsg(socket, name, msg, delay) {
 }; // class Listener {
