59666e894df8bdc05653efff58bd7af88957cb95
CyberT33N <resets.17134633+CyberT33N@users.noreply.github.com>
2020-12-15 01:55:08 +0100

    Beautify Unit Test Client Init.mjs

--- controller/utils/socket.mjs
+++ controller/utils/socket.mjs
@@ -1,5 +1,6 @@
 import {testSocket} from '../../services/utils/socket/testSocket.mjs';
 
+
 export default {
   testSocket: async (listerName, socket, emitName, emitMsg)=>{
     return await testSocket(listerName, socket, emitName, emitMsg);
